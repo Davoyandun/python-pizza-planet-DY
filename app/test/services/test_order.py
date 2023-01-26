028eef6528d9d6f14da050a416997fc879133e48
David Yandún <70674906+Davoyandun@users.noreply.github.com>
2023-01-25 19:51:51 -0500

    fet: add test coverage (#2)

--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -1,13 +1,8 @@
 import pytest
 
-def test_create_order_service(create_orders):
-    for created_order in create_orders:
-        order = created_order.json
-        pytest.assume(created_order.status.startswith('200'))
-        pytest.assume(order['_id'])
-        pytest.assume(order['client_address'])
-        pytest.assume(order['client_dni'])
-        pytest.assume(order['client_name'])
-        pytest.assume(order['client_phone'])
-        pytest.assume(order['ingredients'])
-        pytest.assume(order['size_id'])
+def test_create_order_service(client, order_uri, order):
+    response = client.post(order_uri, json=order)
+    pytest.assume(response.status.startswith('200'))
+    returned_order = response.json
+    for param, value in order.items():
+        pytest.assume(returned_order[param] == value)
